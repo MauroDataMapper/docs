a12bb8b14982e2cf99f2a58772f279f27549d9e2
Gabriel Ignat <gabriel.ignat@bdi.ox.ac.uk>
2020-05-29 11:51:39 +0100

    Added: Favicon, Logo, Colours & Fonts. Updated: header & footer

---
+++ theme/assets/javascripts/extra.js
@@ -0,0 +1,10 @@
+// window.addEventListener("load", function() {
+//     const _get = document.querySelectorAll("endpoint.get");
+//     // this.console.log(_get);
+
+//     for (i = 0; i < _get.length; i++) {
+//         // _get[i].insertAdjacentHTML("afterbegin", '<h1>A cat</h1>');
+//         this.console.log(_get[i].innerHTML);
+//         _get[i].innerHTML = 
+//     }
+// });
